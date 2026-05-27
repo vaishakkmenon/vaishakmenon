7aa5abe3490ae98235765948ef1398cb3c5efd29
Vaishak Menon <vaishakkmenon@gmail.com>
2026-05-27 16:54:40 -0600

    fixed not-found error where it was incorrectly placing a black background

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
 export default function NotFound(): React.ReactElement {
     return (
-        <main className="site-main p-8 text-center">
+        <div className="min-h-dvh flex flex-col items-center justify-center p-8 text-center">
             <h1 className="text-3xl font-bold">Page not found</h1>
             <p>Sorry, we can&apos;t find that page.</p>
-        </main>
+        </div>
     );
 }
